30a37061911ee8cb87fb8ec829f154a621ca16f2
shreeshailmiragi18 <shreeshailbm18@gmail.com>
2026-02-09 22:13:09 +0530

    completed footer of landing page

--- Frontend/src/components/Landing/FooterSection.jsx
+++ Frontend/src/components/Landing/FooterSection.jsx
@@ -1,5 +1,15 @@
 const FooterSection = () => {
-  return <div>Footer Section</div>;
+  return (
+    <footer className="bg-gray-800">
+      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+        <div className="text-center">
+          <p className="text-base text-gray-400">
+            &copy; 2026 CloudShare. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default FooterSection;
